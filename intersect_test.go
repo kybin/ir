2d27d01044edef814b1369d119db5de9512bfd20
kybin <kybinz@gmail.com>
2015-05-31 15:08:15 +0000

    Errorf need args.

--- intersect_test.go
+++ intersect_test.go
@@ -10,10 +10,10 @@ func TestIntersect2(t *testing.T) {
 	b := line2{point2{-1, 0}, point2{2, 1}}
 	got, ok := intersect2(a, b)
 	expect := point2{0.5, 0.5}
-	if !ok{
-		t.Errorf("Intersection of %v and %v is %v, got no intersection.")
+	if !ok {
+		t.Errorf("Intersection of %v and %v is %v, got no intersection.", a, b, expect)
 	}
-	if !ok || math.Abs(expect.x-got.x) > ignorable || math.Abs(expect.y-got.y) > ignorable {
+	if math.Abs(expect.x-got.x) > ignorable || math.Abs(expect.y-got.y) > ignorable {
 		t.Errorf("Intersection of %v and %v is %v, got %v instead.", a, b, expect, got)
 	}
 }
