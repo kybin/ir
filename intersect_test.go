4a2ab93f722a0f1cea87d431b7350f628441d4a4
yongbin kim <kybinz@gmail.com>
2015-06-03 00:56:10 +0900

    add more test for intersect.

--- intersect_test.go
+++ intersect_test.go
@@ -16,4 +16,22 @@ func TestIntersect2(t *testing.T) {
 	if math.Abs(expect.x-got.x) > ignorable || math.Abs(expect.y-got.y) > ignorable {
 		t.Errorf("Intersection of %v and %v is %v, got %v instead.", a, b, expect, got)
 	}
+
+	a = line2{point2{0, -1}, point2{0, 1}}
+	b = line2{point2{-1, 0}, point2{1, 0}}
+	got, ok = intersect2(a, b)
+	expect = point2{0, 0}
+	if !ok {
+		t.Errorf("Intersection of %v and %v is %v, got no intersection.", a, b, expect)
+	}
+	if math.Abs(expect.x-got.x) > ignorable || math.Abs(expect.y-got.y) > ignorable {
+		t.Errorf("Intersection of %v and %v is %v, got %v instead.", a, b, expect, got)
+	}
+
+	a = line2{point2{2,1}, point2{3,3}}
+	b = line2{point2{2,-1}, point2{3,-3}}
+	got, ok = intersect2(a, b)
+	if ok {
+		t.Errorf("Intersection of %v and %v shold not have itersection. got %v", a, b, got)
+	}
 }
