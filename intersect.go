9bfd4fc7b989b05ff4a829c2406c660620875263
yongbin kim <kybinz@gmail.com>
2015-06-03 00:51:15 +0900

    reimplement intersect2.

--- intersect.go
+++ intersect.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"math"
+)
+
 type point2 struct {
 	x, y float64
 }
@@ -16,19 +20,37 @@ func slopeIntercept(l line2) (float64, float64) {
 	return slope, intercept
 }
 
+func swapXY(l line2) line2 {
+	l.start.x, l.start.y = l.start.y, l.start.x
+	l.end.x, l.end.y = l.end.y, l.end.x
+	return l
+}
+
 // caculate two 2d line intersection point.
 func intersect2(a, b line2) (point2, bool) {
+	swaped := false
+	xlen := math.Abs(a.end.x-a.start.x)
+	ylen := math.Abs(a.end.y-a.start.y)
+	if ylen > xlen {
+		a = swapXY(a)
+		b = swapXY(b)
+		swaped = true
+	}
 	sa, ia := slopeIntercept(a)
-	sb, ib := slopeIntercept(b)
-	if sa == sb {
+	// shear b to y direction.
+	b.start.y = b.start.y - (sa*b.start.x) - ia
+	b.end.y = b.end.y - (sa*b.end.x) - ia
+	if math.Signbit(b.start.y) == math.Signbit(b.end.y) {
 		return point2{}, false
 	}
-	slope := sb - sa
-	intercept := ib - ia
-	x := -intercept/slope
-	if x < a.start.x || x > a.end.x || x < b.start.x || x > b.end.x {
-		return point2{}, false
+	// find x if y == 0
+	tb := b.start.y / math.Abs(b.end.y - b.start.y)
+	x := tb * math.Abs(b.end.x - b.start.x) + b.start.x
+	// b is sheared. so find y with a.
+	ta := x / math.Abs(a.end.x - a.start.x)
+	y := ta / (a.end.x - a.start.x)
+	if swaped {
+		x, y = y, x
 	}
-	y := sa*x + ia
 	return point2{x, y}, true
 }
