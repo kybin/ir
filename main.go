8f7a69c2e73dcb3d4397235702b68a88868b23df
yongbin kim <kybinz@gmail.com>
2015-06-30 01:18:13 +0900

    simple rotate box to camera.

--- main.go
+++ main.go
@@ -7,12 +7,22 @@ import (
 
 func main() {
 	geo := loadGeometry()
-	modelTransform := matrix4{
-		math.Cos(pi / 4), 0, -math.Sin(pi / 4), 0,
+
+	// TODO : rotation matrix
+	// rotation by (x:30, y:30) degree.
+	yRot := matrix4{
+		math.Cos(pi / 6), 0, -math.Sin(pi / 6), 0,
 		0, 1, 0, 0,
-		math.Sin(pi / 4), 0, math.Cos(pi / 4), 0,
-		1, -1, -4, 1,
+		math.Sin(pi / 6), 0, math.Cos(pi / 6), 0,
+		0, 0, 0, 1,
 	}
+	xRot := matrix4{
+		1, 0, 0, 0,
+		0, math.Cos(pi / 6), math.Sin(pi / 6), 0,
+		0, -math.Sin(pi / 6), math.Cos(pi / 6), 0,
+		0, 0, 0, 1,
+	}
+	modelTransform := xRot.Multiply(yRot).Transpose()
 
 	// view transform should inverse of camera transform
 	// for easy reversing, think it as rotation+translation.
@@ -23,7 +33,7 @@ func main() {
 	//
 	// TODO : redefine camera.
 	cam := &camera{
-		P:     vector3{0, 0, 5},
+		P:     vector3{0, 0, 10},
 		front: vector3{0, 0, -1},
 		right: vector3{1, 0, 0},
 		up:    vector3{0, 1, 0},
@@ -35,10 +45,10 @@ func main() {
 		far:   10000,
 	}
 	viewTranslation := matrix4{
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		-cam.P.x, -cam.P.y, -cam.P.z, 1,
+		1, 0, 0, -cam.P.x,
+		0, 1, 0, -cam.P.y,
+		0, 0, 1, -cam.P.z,
+		0, 0, 0, 1,
 	}
 	viewRotation := matrix4{
 		cam.right.x, cam.up.x, cam.front.x, 0,
@@ -48,18 +58,20 @@ func main() {
 	}
 	viewTransform := viewTranslation.Multiply(viewRotation)
 
+
+	geo.Transform(viewTransform.Multiply(modelTransform))
+
+	// TODO : do I need perspective projection in pbr renderer?
 	// perspective projection
 	// http://ogldev.atspace.co.uk/www/tutorial12/tutorial12.html
-	perspProjection := matrix4{
-		1 / cam.Apty(), 0, 0, 0,
-		0, 1 / cam.aptx, 0, 0,
-		0, 0, (-cam.near - cam.far) / (cam.near - cam.far), 2 * cam.far * cam.near / (cam.near - cam.far),
-		0, 0, 1, 0,
-	}
+	// perspProjection := matrix4{
+	//	1 / cam.Apty(), 0, 0, 0,
+	//	0, 1 / cam.aptx, 0, 0,
+	//	0, 0, (-cam.near - cam.far) / (cam.near - cam.far), 2 * cam.far * cam.near / (cam.near - cam.far),
+	//	0, 0, 1, 0,
+	//}
 
-	// TODO : to ndc?
-
-	geo.Transform(perspProjection.Multiply(viewTransform.Multiply(modelTransform)))
+	// geo.Transform(perspProjection.Multiply(viewTransform.Multiply(modelTransform)))
 
 	render(geo)
 }
@@ -103,12 +115,12 @@ func loadGeometry() geometry {
 	}
 
 	return geometry{
-		&top,
-		&front,
-		&left,
-		&right,
 		&back,
+		&left,
+		&front,
+		&right,
 		&bottom,
+		&top,
 	}
 }
 
