b9565bf4c7704a53aff82edb3822ef8b3330a6b2
yongbin kim <kybinz@gmail.com>
2015-08-31 07:22:43 +0900

    -1 to max tex coord

--- tex.go
+++ tex.go
@@ -24,8 +24,8 @@ func LoadTexture(pth string) (image.Image, bool) {
 
 func TextureSample(tex image.Image, u, v float64) color.RGBA {
 	bb := tex.Bounds()
-	x := int(float64(bb.Min.X) + u*float64(bb.Max.X-bb.Min.X))
-	y := int(float64(bb.Min.Y) + v*float64(bb.Max.Y-bb.Min.Y))
+	x := int(float64(bb.Min.X) + u*float64(bb.Max.X-bb.Min.X-1))
+	y := int(float64(bb.Min.Y) + v*float64(bb.Max.Y-bb.Min.Y-1))
 	r, g, b, a := tex.At(x, y).RGBA()
 	return color.RGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)}
 }
